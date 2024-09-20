98c68701408a969653da7bb61aebeff2d5c1818f
Yuriel <yurielnf@gmail.com>
2024-09-20 16:01:49 +0200

    add green non interacting (broken)

---
+++ example/free_fermion_green.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <armadillo>
+
+#include <irlm.h>
+
+
+using namespace std;
+using namespace arma;
+
+
+int main(int argc, char **argv)
+{
+    cout<<"Hola mundo "<<setprecision(15)<<endl;
+
+    int len=20;
+    double dt=0.1;
+    if (argc>=2) len=atoi(argv[1]);
+    if (argc>=3) dt=atof(argv[2]);
+
+    auto model1=IRLM {.L=len, .t=0.5, .V=0.1, .U=0.0, .ed=0.0};
+
+    arma::mat H1=model1.matrices().first;
+
+    arma::vec eval1;
+    arma::mat evec1;
+
+    eig_sym(eval1,evec1,H1);
+
+    auto green=[&](int i, int j, double t, bool is_greater) {
+        cx_mat phase=diagmat(arma::exp(eval1*cx_double(0,-t)));
+        double Ef=arma::sum(eval1(arma::find(eval1<0)));
+        cx_rowvec jket=evec1.row(j)*phase*evec1.t()*evec0;
+        cx_rowvec iket=evec1.row(i)*phase*evec1.t()*evec0;
+        cx_double sum=0;
+        for(auto a=0; a<eval0.size(); a++)
+           if (eval0[a]<0) sum += std::conj(iket[a])*jket[a];
+        return sum;
+    };
+
+    ofstream out("free_L"s+to_string(len)+".txt");
+    out<<"time M m energy n0\n"<<setprecision(14);
+    out<<"0 0 0 0 "<< corr(0,0,0).real() <<" "<< (corr(0,1,0)+corr(1,0,0)).real() <<endl;
+
+    for(auto i=0; i*dt<=len; i++) {
+        double t=dt*(i+1);
+        out<<t<<" 0 0 0 "<< corr(0,0,t).real() <<" "<< (corr(0,1,t)+corr(1,0,t)).real() <<endl;
+    }
+
+    return 0;
+}
