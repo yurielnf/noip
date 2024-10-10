44260840fd482a5a68d9816e691ba5b221655cf6
Yuriel <yurielnf@gmail.com>
2024-10-10 17:38:02 +0200

    fix conjugate missing

--- example/free_fermion_green.cpp
+++ example/free_fermion_green.cpp
@@ -29,25 +29,25 @@ int main(int argc, char **argv)
     arma::mat evec1;
 
     eig_sym(eval1,evec1,H1);
+    cout<<"energy "<<arma::sum(eval1(arma::find(eval1<0)))<<endl;
 
     auto green=[&](int i, int j, double t, bool is_greater) {
-        cx_mat phase=diagmat(arma::exp(eval1*cx_double(0,-t)));
-        double Ef=arma::sum(eval1(arma::find(eval1<0)));
+        cx_vec phase=arma::exp(eval1*cx_double(0,-t));
         rowvec jket=evec1.row(j);
-        cx_rowvec iket=evec1.row(i)*phase;
+        rowvec iket=evec1.row(i);
         cx_double sum=0;
         for(auto a=0; a<eval1.size(); a++)
            if ((is_greater && eval1[a]>=0) ||
-               (!is_greater && eval1[a]<0)) sum += std::conj(iket[a])*jket[a];
+                (!is_greater && eval1[a]<0)) sum += std::conj(iket[a])*jket[a]*phase(a);
         return sum;
     };
 
     ofstream out("free_green_L"s+to_string(len)+"_g"+to_string(is_greater)+".txt");
-    out<<"time M m energy n0\n"<<setprecision(14);
+    out<<"time mBra mKet greenRe greenIm nActiveBra nActiveKet\n"<<setprecision(14);
     for(auto i=0; i*dt<=len; i++) {
         double t=dt*i;
         cx_double g=green(0,0,t,is_greater);
-        out<<t<<" 0 0 0 "<< g.real() <<" "<< g.imag()<< " 0 0"<<endl;
+        out<<t<<" 1 1 "<< g.real() <<" "<< g.imag()<< " 0 0"<<endl;
     }
 
     return 0;
